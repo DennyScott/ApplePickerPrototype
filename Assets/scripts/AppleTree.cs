1542f9f6f0ec216dc79115eadfb16bcece21bb7b
Denny Scott <dennyscott301@gmail.com>
2014-11-11 20:14:21 -0600

    Inital Commit

---
+++ Assets/scripts/AppleTree.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour
+{
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
